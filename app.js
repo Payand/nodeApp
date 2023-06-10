086395414d9ede91225ad3c3b1181bc6d5c5cff5
Ali Shooshtari <shooshtari@hasingroup.com>
2023-06-10 19:17:39 +0330

    initial comment

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const app = express();
 
 
 //middlewars
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV)
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 app.use((req, res, next) => {
